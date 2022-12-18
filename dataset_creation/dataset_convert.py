768c950c6f6424c1dbc048d8ea4ae91ff4e077bb
bmarvin <beesemarvin@gmail.com>
2022-12-18 19:31:51 +0100

    minor refactorings: added docstrings, typehints for methods, argument-safety for read_data and write_data

--- dataset_creation/dataset_convert.py
+++ dataset_creation/dataset_convert.py
@@ -5,7 +5,6 @@ import sys
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
-from typing import List
 
 
 def parse_args():
@@ -18,27 +17,34 @@ def parse_args():
         exit(1)
 
 
-def convert(df):
+def convert(df: pd.DataFrame) -> pd.DataFrame:
+    ''' converts coordinates and returns them serialized into columns
+    '''
     # add converted coordinates column
-    print("Converting coordinates:")
-    print(df)
+    print("\tConverting coordinates")
     coords_tuples = df.apply(lambda row: convert_coordinates((row[1], row[2]), (row[3], row[4])), axis=1)
     coords_tuples = coords_tuples.apply(pd.Series)
     df.iloc[:, 1:5] = coords_tuples
-    print('\nConverted coordinates:')
-    print(df)
     return df
 
 
-def read_data():
-    # import data
+def read_data(in_file: str) -> pd.DataFrame:
+    ''' reads data frome a relative or absolute path as a String,
+        separates data with ';' \n
+        returns dataframe
+    '''
+    print('\tReading data from input file')
     df = pd.read_csv(in_file, sep=';', header=None)
     return df
 
 
-def write_data(df):
+def write_data(df: pd.DataFrame, out_dir: str):
+    ''' writes out data of each picture into a separate file,
+        appends multiple labels of the same picture into the 
+        same output file using newline as a delimiter
+    '''
     # write each row into a separate file
-    print("Writing data to files:")
+    print("\tWriting data to files")
     os.makedirs(out_dir, exist_ok=True)
     for index, row in df.iterrows():
         file_name = row[0].replace(".ppm", ".txt")
@@ -50,12 +56,17 @@ def write_data(df):
         else:
             with open(file_path, 'w') as f:
                 f.write(f"{row[5]} {row[1]} {row[2]} {row[3]} {row[4]}")
-        print(f"File {file_name} written.")
-    #df.to_csv(out_file, sep=' ', header=None, index=None)
+        # print(f"File {file_name} written.")
 
 
-def convert_coordinates(top_left, bottom_right):
-    # convert coordinates to center coordinate and width and height of bounding box from pixels to percentage
+def convert_coordinates(top_left: list, bottom_right: list) -> list:
+    ''' top_left: [leftmost image: float, upmost image: float]\n
+        bottom_right: [rightmost image: float, downmost image]\n
+        returns: [x_center: float, y_center: float, width: float, height: float]
+
+        conversion of given coordinates to center coordinates for x,y and with, height
+        of the bounding box with normalization to [0,1] to a percentage value.
+    '''
     x_center = (top_left[0] + bottom_right[0]) / 2 / pixels_x
     y_center = (top_left[1] + bottom_right[1]) / 2 / pixels_y
     width = (bottom_right[0] - top_left[0]) / pixels_x
@@ -65,9 +76,8 @@ def convert_coordinates(top_left, bottom_right):
 
 def annotate_sample(sample_path: str, annotation_path: str):
     """ Loads a sample from `sample_path` and an annotation from `annotation_path`.
-    Than draws the BBoxs from the annotations on the image and save the image to 
+        Then draws the BBoxs from the annotations on the image and saves the image too 
     """
-
     with open(annotation_path, encoding="utf8", mode="r") as f:
         bboxs = [[float(__a) for __a in _a.split(" ")] for _a in f.read().split("\n")]
 
@@ -95,7 +105,7 @@ def annotate_sample(sample_path: str, annotation_path: str):
 
 if __name__ == '__main__':
     in_file, out_dir, pixels_x, pixels_y = parse_args()
-    df = read_data()
+    df = read_data(in_file)
     convert(df)
-    write_data(df)
+    write_data(df, out_dir)
     # annotate_sample(sample_path, annotation_path)
