505f6424cf823d0444cf08b38411fb1ff26a42d9
dario windows desktop <daapplab@gmail.com>
2023-01-08 21:06:24 +0100

    added batch scripts how yolo v5 was trained

---
+++ train_yolov5/train_yolo_xl.bat
@@ -0,0 +1,11 @@
+
+
+
+
+..\.venv\Scripts\python.exe ..\yolov5\train.py ^
+    --data data.yaml ^
+    --epochs 300 ^
+    --weights yolov5x.pt ^
+    --batch -1 ^
+    --cache ram ^
+    --device 0
